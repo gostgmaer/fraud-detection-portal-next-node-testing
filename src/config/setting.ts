238d7865d09dcf281d0fd00277c95d7e0ad5132a
Kishor Sarkar <kishor.sarkar@inadev.com>
2023-12-28 12:50:22 +0530

    updated code

---
+++ src/config/setting.ts
@@ -0,0 +1,6 @@
+export const containerId = process.env.NEXT_PUBLIC_CONTAINER
+export const tableId = process.env.NEXT_PUBLIC_TABLE
+export const contactTable = "contacts"
+export const mongourl = process.env.MONGODB_URI
+export const baseurl = process.env.NEXT_PUBLIC_BASE_URL
+export const secret = process.env.NEXT_PUBLIC_SECRET
